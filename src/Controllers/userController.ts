f4ff2d809f71f8b5bf23c2e55bec5b64951da90b
Dawatenzingsherpa <dawatenzingsherpa907@gmail.com>
2026-06-13 16:38:29 +0545

    loginUser and token

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request,Response } from "express";
 import User from "../Database/models/userModel";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 class AuthController{
   public static async registerUser(req:Request,res:Response):Promise<void>{
@@ -23,6 +24,53 @@ class AuthController{
     res.status(201).json({
       message : "User registered successfully"
     })
+
+  }
+
+  public static async loginUser(req: Request,res:Response):Promise<void>{
+    //user input 
+    const {email,password} = req.body;
+    if(!email || !password){
+      res.status(400).json({
+        message : "please provide email and password"
+      })
+      return 
+    }
+
+    //check if user with above email exist or not 
+    const [data] = await User.findAll({
+      where : {
+        email : email
+      }
+    })
+    if(!data){
+      res.status(404).json({
+        message : "no user with that email"
+      })
+      return
+    }
+
+    //check password
+    const isMatched = bcrypt.compareSync(password,data.password);
+
+    if(!isMatched){
+      res.status(403).json({
+        message : "Invalid email or password"
+      })
+      return 
+    }
+
+    if(isMatched){
+      const token = jwt.sign({id:data.id},"hahaha",{
+        expiresIn : "20d"
+      })
+
+      res.status(200).json({
+        message : "Logged in successfully",
+        data : token
+      })
+    }
+
 
   }
 }
